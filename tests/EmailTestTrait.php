71557d680471784d789737bfe2d2c913d2375529
nclifton <neil.clifton@cliftonwebfoundry.com.au>
2016-08-22 22:59:12 +1000

    password reset test including email

--- tests/EmailTestTrait.php
+++ tests/EmailTestTrait.php
@@ -59,4 +59,73 @@ trait EmailTestTrait
     {
         $this->assertNotEmpty($this->getMessages(), $description);
     }
+
+    public function assertEmailSubjectContains($needle, $email, $description = '')
+    {
+        $this->assertContains($needle, $email->subject, $description);
+    }
+
+    public function assertEmailSubjectEquals($expected, $email, $description = '')
+    {
+        $this->assertContains($expected, $email->subject, $description);
+    }
+
+    public function assertEmailHtmlContains($needle, $email, $description = '')
+    {
+        $response = $this->getEmailHtml($email);
+        $this->assertContains($needle, (string)$response->getBody(), $description);
+    }
+
+    public function assertEmailTextContains($needle, $email, $description = '')
+    {
+        $response = $this->mailcatcher->get("/messages/{$email->id}.plain");
+        $this->assertContains($needle, (string)$response->getBody(), $description);
+    }
+
+    public function assertEmailSenderEquals($expected, $email, $description = '')
+    {
+        $response = $this->mailcatcher->get("/messages/{$email->id}.json");
+        $email = json_decode($response->getBody());
+        $this->assertEquals($expected, $email->sender, $description);
+    }
+
+    public function assertEmailRecipientsContain($needle, $email, $description = '')
+    {
+        $response = $this->mailcatcher->get("/messages/{$email->id}.json");
+        $email = json_decode($response->getBody());
+        $this->assertContains($needle, $email->recipients, $description);
+    }
+
+    /**
+     * @param $email
+     * @return \GuzzleHttp\Message\FutureResponse|\GuzzleHttp\Message\ResponseInterface|\GuzzleHttp\Ring\Future\FutureInterface|null
+     */
+    private function getEmailHtml($email)
+    {
+        $response = $this->mailcatcher->get("/messages/{$email->id}.html");
+        return $response;
+    }
+
+    /**
+     * @param $email
+     * @return array
+     */
+    protected function getLinksInEmailHtml($email)
+    {
+        $doc = new \DOMDocument();
+        libxml_use_internal_errors(true);
+        $doc->loadHTML($this->getEmailHtml($email));
+        $links = [];
+        $arr = $doc->getElementsByTagName("a"); // DOMNodeList Object
+        foreach ($arr as $item) { // DOMElement Object
+            $href = $item->getAttribute("href");
+            $text = trim(preg_replace("/[\r\n]+/", " ", $item->nodeValue));
+            $links[] = [
+                'href' => $href,
+                'text' => $text
+            ];
+        }
+        return $links;
+    }
+
 }
--- tests/selenium-tests/PasswordResetTest.php
+++ tests/selenium-tests/PasswordResetTest.php
@@ -38,11 +38,26 @@ class PasswordResetTest extends IntegratedTestCase
         $this->assertThat($this->byCssSelector('.panel-heading')->text(), $this->equalTo('Reset Password'));
         $this->clearType($user['email'],'email');
         $this->byCssSelector('button.btn.btn-primary')->click();
+        $this->assertThat($this->byCssSelector('.alert.alert-success')->text(),$this->equalTo('We have e-mailed your password reset link!'));
+        $this->closeBrowser();
 
         $this->assertEmailIsSent();
+        $email = $this->getLastMessage();
+        $this->assertEmailSubjectContains('Your Password Reset Link',$email);
+        $this->assertEmailHtmlContains('href="http://local.truckertracker.services/password/reset',$email);
+
+        $links = $this->getLinksInEmailHtml($email);
+        $this->assertThat(array_column($links,'text'),$this->contains('Reset your password'));
+        $this->visit($links[array_search('Reset your password', array_column($links,'text'))]['href']);
+
+        $this->assertThat($this->byCssSelector('.panel-heading')->text(), $this->equalTo('Reset Password'));
+
+        $this->assertTrue($this->byId('password')->displayed());
+        $this->assertTrue($this->byId('password-confirm')->displayed());
+        $this->assertThat($this->byCssSelector('button.btn.btn-primary')->text(),$this->equalTo('Reset Password'));
+
 
     }
-
 
 
 }
